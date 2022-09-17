218c4c55b9f9fbeca9fd703f0194e795d04747be
DanielAlfasi <danielalfasi2@gmail.com>
2022-09-17 18:44:21 +0300

    nothing

--- react-app/src/components/AllVehicles.js
+++ react-app/src/components/AllVehicles.js
@@ -2,24 +2,31 @@ import React from "react";
 import Vehicle from "./Vehicle";
 import axios from "axios";
 
-// var getVehicles = async()=>{
-//     await axios.get('http://localhost:4000/vehicles',{
-//         headers:{
-//             Authorization: 'Bearer '+ JSON.parse(localStorage.getItem('Token'))
-//         }
-//     }).then((response)=>{
-//         console.log(response);
-//     }).catch((error)=>{
-//         console.log(error);
-//     })
-// }
+var getVehicles = async()=>{
+    await axios.get('http://localhost:4000/vehicles',{
+        headers:{
+            Authorization: 'Bearer '+ JSON.parse(localStorage.getItem('Token'))
+        }
+    }).then((response)=>{
+        console.log(response.data);
+        // return setVehicle(response.data)
+    }).catch((error)=>{
+        console.log(error);
+    })
+}
+
 
 export default function AllVehicles(){
-
+    // const [vehicle , setVehicle] = React.useState(null)
+    // getVehicles()
+    // function setVehicle(data) {
+    //     vehicle = data
+    // }
+    getVehicles()
     return(
         <div>
         {
-
+            
         }
         </div>
     );
