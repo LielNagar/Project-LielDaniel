475e8559bcb0b68aee6272a3d8bfec70bc886cbc
LielNagar <104072448+LielNagar@users.noreply.github.com>
2022-09-19 20:23:25 +0300

    Index Filter have query

--- react-app/src/components/indexFilter.js
+++ react-app/src/components/indexFilter.js
@@ -1,8 +1,36 @@
-import React, { useState } from "react";
+import React from "react";
 import axios from 'axios';
+//import Swal from "sweetalert2";
 
 function Submit(){
-  window.location.href='/Results';
+  // const thisYear=new Date().getFullYear();
+  // const year= document.getElementById('year').value;
+  // if(year)
+  //   if(year>thisYear || year<2000){
+  //     return Swal.fire({
+  //       icon: 'error',
+  //       title: 'Year Problem!',
+  //       text: 'In order to search a vehicle you have to apply a year between 2000 and '+ thisYear+ '. Or not at all'});
+  //   } ******TO COMPLETE LATER********
+  const manufacturer=document.getElementById('manufacturer').value;
+  const gear= document.getElementById('gear').value;
+  const AC= document.getElementById('AC').checked;
+  const blueTooth= document.getElementById('BT').checked;
+  const GPS= document.getElementById('GPS').checked;
+  let params={};
+  if(manufacturer === 'Select A Manufacturer')
+    if(gear === 'Select Gear Type')
+      params={AC, blueTooth, GPS};
+    else params={AC, blueTooth, GPS, gear};
+  else if (gear === 'Select Gear Type') params={AC, blueTooth, GPS, manufacturer};
+  else params={AC,blueTooth,GPS,manufacturer,gear};
+  console.log(params);
+  axios.get('http://localhost:4000/vehicles',{params}).then((response)=>{
+    //window.location.href='/Results';
+    console.log(response);
+  }).catch((error)=>{
+    console.log(error);
+  })
 }
 export default class IndexFilter extends React.Component{
   state={
@@ -21,18 +49,18 @@ export default class IndexFilter extends React.Component{
       <div>
         <input id='sDate' type='date'></input>
         <input id='eDate' type='date'></input>
-        <select>
+        <select id='manufacturer'>
           <option>Select A Manufacturer</option>
           {this.state.manufacturers.map((manufacturer)=>{
-            return <option key={manufacturer}>{manufacturer}</option>
+            return <option key={manufacturer} id={manufacturer} value={manufacturer}>{manufacturer}</option>
           })}
         </select>
-        <select>
+        <select id='gear'>
           <option>Select Gear Type</option>
           <option>Auto</option>
           <option>Manual</option>
         </select>
-        <input id='year' type='number' min='1990' max={new Date().getFullYear()} placeholder='Year Above'></input>
+        <input id='year' type='number' min='2000' max={new Date().getFullYear()} placeholder='Year Above'></input>
         <br></br>
         <label>Do you want AC?</label>
         <input id='AC' type='checkbox'></input>
